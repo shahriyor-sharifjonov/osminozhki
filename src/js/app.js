abb0386156993462d1de14fda1cc05ca45734b29
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-08-30 12:25:52 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ import * as animations from "./modules/animations.js";
 import * as header from "./modules/header.js";
 import * as smoothscroll from "./modules/smoothscroll.js";
 import * as swipers from "./modules/swipers.js";
+import * as acc from "./modules/acc.js";
 
 functions.isWebp();
 animations.init();
 header.init();
 smoothscroll.init();
 swipers.init();
+acc.init();
